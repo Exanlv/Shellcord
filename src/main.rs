63522c36ff17a00d3e47d4a97c0a99eb7e24ddbb
Exan <exan@landviz.nl>
2020-12-28 17:00:03 +0100

    Add send message support

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,57 @@
 use std::env;
 use std::process::Command;
 use std::process::Output;
+use serde_json::json;
 
 use serenity::{
     async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use std::collections::HashMap;
+use regex::Regex;
+use serenity::http::CacheHttp;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    async fn message(&self, _ctx: Context, msg: Message) {
-		let mut args = Vec::new();
-		args.push(format!("ID=\"{}\"", msg.id.as_u64()));
-		args.push(format!("AUTHOR_ID=\"{}\"", msg.author.id.as_u64()));
-		args.push(format!("AUTHOR_DISCRIMINATOR=\"{}\"", msg.author.discriminator));
-		args.push(format!("AUTHOR_NAME=\"{}\"", msg.author.name));
-		args.push(format!("CHANNEL_ID=\"{}\"", msg.channel_id.as_u64()));
-		args.push(format!("CONTENT=\"{}\"", msg.content));
+    async fn message(&self, ctx: Context, msg: Message) {
+		let mut args = HashMap::new();
+
+		args.insert(
+			"ID".to_string(),
+			msg.id.as_u64().to_string()
+		);
+
+		args.insert(
+			"AUTHOR_ID".to_string(),
+			msg.author.id.as_u64().to_string()
+		);
+
+		args.insert(
+			"AUTHOR_DISCRIMINATOR".to_string(),
+			msg.author.discriminator.to_string()
+		);
+
+		args.insert(
+			"AUTHOR_NAME".to_string(),
+			msg.author.name
+		);
+
+		args.insert(
+			"CHANNEL_ID".to_string(),
+			msg.channel_id.as_u64().to_string()
+		);
+
+		args.insert(
+			"CONTENT".to_string(),
+			msg.content
+		);
 
 		let output = run_command("message", args, ctx).await;
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -42,28 +59,20 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .await
         .expect("Err creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
 }
 
-async fn run_command(script_name: &str, args: Vec<String>, ctx: Context) {
+async fn run_command(script_name: &str, args: HashMap<String, String>, ctx: Context) {
 	let output = if cfg!(target_os = "windows") {
 		// windows LOL
 		Command::new("cmd")
@@ -71,15 +80,15 @@ async fn run_command(script_name: &str, args: Vec<String>, ctx: Context) {
 				.output()
 				.expect("failed to execute process")
 	} else {
-		Command::new("bash")
+		Command::new("sh")
 			.arg(script_name)
-			.args(args)
+			.envs(args)
 			.output()
             .expect("failed to execute process")
 	};
 
 	if output.status.success() {
-		handle_output(output).await;
+		handle_output(output, ctx).await;
 	} else {
 		println!("ERROR {:?}", output);
 	}
@@ -88,11 +97,26 @@ async fn run_command(script_name: &str, args: Vec<String>, ctx: Context) {
 async fn handle_output(output: Output, ctx: Context) {
 	let mut stdout = String::from_utf8(output.stdout).unwrap();
 
+	if stdout.is_empty() {
+		return;
+	}
+
 	stdout.pop();
 
 	let commands = stdout.split("\n");
 
 	for command in commands {
-		println!("{:?}", command);
+		let regex = Regex::new(r"^SEND_MESSAGE (\d*) (.*)$").unwrap();
+
+		let matches = regex.captures(command).unwrap();
+
+		let json = json!({
+			"content": matches.get(2).map_or("", |m| m.as_str())
+		});
+
+		ctx.http().send_message(
+			matches.get(1).map_or("", |m| m.as_str()).parse::<u64>().unwrap(),
+			&json
+		).await;
 	}
 }
